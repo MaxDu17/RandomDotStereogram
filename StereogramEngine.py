4fba00ae659744d8008680d70aac183e2dab000f
MaxDu <maximilianjdu@gmail.com>
2022-10-18 21:34:24 -0700

    added animations

--- StereogramEngine.py
+++ StereogramEngine.py
@@ -2,40 +2,92 @@ import matplotlib.pyplot as plt
 import numpy as np
 import imageio
 from skimage.transform import resize
+import io
+from scipy.ndimage.interpolation import rotate
 
 NUMBER_POINTS = 5000
 SIZE = 1
 RESOLUTION = 512
 
 
-im = imageio.imread("test.png")
-im = resize(im, (512, 512))[:, :, :-1] #remove the alpha channel
-im = np.mean(im, axis = 2)
-mask = im < 0.5
-mask = np.rot90(mask, k = 3)
 
 
-x_list = np.random.random((NUMBER_POINTS)) * RESOLUTION
-y_list = np.random.random((NUMBER_POINTS)) * RESOLUTION
-shifted_x_list = x_list.copy()
-shifted_x_list -= 0.01 * RESOLUTION
-
-for i in range(len(x_list)):
-    nearest_x = int(x_list[i])
-    nearest_y = int(y_list[i])
-    if mask[nearest_x, nearest_y]:
-        shifted_x_list[i] -= 0.01 * RESOLUTION
-
-    # if 0.25 < x_list[i] < 0.75 and 0.25 < y_list[i] < 0.75:
-    #     shifted_x_list[i] -= 0.01
-
-plt.scatter(x_list, y_list, s = SIZE, color = "#3AF2F8", alpha = 0.8)
-plt.scatter(shifted_x_list - 0.01, y_list, s = SIZE, color = "#ff370f", alpha = 0.8)
-
-plt.show()
 
 
-# for i in range(NUMBER_POINTS):
-#     points_list
-#
+# for i in range(len(x_list)):
+#     nearest_x = int(x_list[i])
+#     nearest_y = int(y_list[i])
+#     if mask[nearest_x, nearest_y]:
+#         shifted_x_list[i] -= 0.01 * RESOLUTION
 
+def make_random_scatter(depth_map):
+    # depth map should go from 0 to 1
+    x_list = np.random.random((NUMBER_POINTS)) * RESOLUTION
+    y_list = np.random.random((NUMBER_POINTS)) * RESOLUTION
+    shifted_x_list = x_list.copy()
+    shifted_x_list -= 0.005 * RESOLUTION
+
+    for i in range(len(x_list)):
+        radius_from_center = np.sqrt((x_list[i] - RESOLUTION / 2)**2 + (y_list[i] - RESOLUTION / 2)**2)
+        nearest_x = int(x_list[i])
+        nearest_y = int(y_list[i])
+        shifted_x_list[i] -= 0.015 * depth_map[nearest_x, nearest_y] * RESOLUTION #radius_from_center
+
+    return x_list, shifted_x_list, y_list
+
+def rotate_image(filename, savename):
+    im = imageio.imread(filename)
+    im = resize(im, (RESOLUTION, RESOLUTION))[:, :, :-1]  # remove the alpha channel
+    im = np.mean(im, axis=2)
+    depth_map = 1 - im
+    depth_map = np.rot90(depth_map, k=3).astype(np.float32)
+
+    writer = imageio.get_writer(f"{savename}.gif", fps=20)
+    for i in range(100):
+        rotated_map = rotate(depth_map, angle=(i / 100) * 359)
+        print(i)
+        fig, ax = plt.subplots()
+        x_list, shifted_x_list, y_list = make_random_scatter(rotated_map)
+        ax.scatter(x_list, y_list, s=SIZE, color="#3AF2F8", alpha=0.8)
+        ax.scatter(shifted_x_list, y_list, s=SIZE, color="#ff370f", alpha=0.8)
+        with io.BytesIO() as buff:
+            fig.savefig(buff, format='raw')
+            buff.seek(0)
+            w, h = fig.canvas.get_width_height()
+            data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+            im = data.reshape((int(h), int(w), -1))
+            writer.append_data(im)  # grayscale rendering
+        plt.close(fig)
+
+    writer.close()
+
+def depth_demo(savename):
+    frequency = 3
+    input_map = 2 * 3.14 * frequency * np.arange(RESOLUTION) / RESOLUTION
+    input_map = np.tile(np.expand_dims(input_map, axis = 0), (RESOLUTION, 1))
+    cycles_in_anim = 10
+
+    writer = imageio.get_writer(f"{savename}.gif", fps=20)
+    for i in range(100):
+        input_map += (2 * 3.14 * cycles_in_anim) * (1 / 100)
+        depth_map = (np.sin(input_map) + 1) / 2
+        print(i)
+        fig, ax = plt.subplots()
+        x_list, shifted_x_list, y_list = make_random_scatter(depth_map)
+        ax.scatter(x_list, y_list, s=SIZE, color="#3AF2F8", alpha=0.8)
+        ax.scatter(shifted_x_list, y_list, s=SIZE, color="#ff370f", alpha=0.8)
+        with io.BytesIO() as buff:
+            fig.savefig(buff, format='raw')
+            buff.seek(0)
+            w, h = fig.canvas.get_width_height()
+            data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+            im = data.reshape((int(h), int(w), -1))
+            writer.append_data(im)  # grayscale rendering
+        plt.close(fig)
+
+    writer.close()
+
+
+if __name__ == "__main__":
+    depth_demo("sine")
+    # rotate_image("whale.png", "bouncing_whale")
